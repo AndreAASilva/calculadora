ce74066d52e8cf5f32ed9163646d00e31e3885d3
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-06-30 04:27:14 -0300

    Pendente inserir valores no display

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-function sete(sete){
+    function hum(){
+        var NumUm = 1
+        console.log(NumUm)            
 
-    alert(7)
-}
+    }
 
+    function dois(){
+        var NumDois = 2
+        console.log(NumDois)
+    }
+
+    function tres(){
+        var NumTres = 3
+        console.log(NumTres)
+    }
+
+    function quatro(){
+        var NumQuatro = 4
+        console.log(NumQuatro)
+    }
+
+    function cinco(){
+        var NumCinco = 5
+        console.log(NumCinco)
+    }
+
+    function seis(){
+        var NumSeis = 6
+        console.log(NumSeis)
+    }
+
+    function sete(){
+        var NumSete = 7
+        console.log(NumSete)
+    }
+
+    function oito(){
+        var NumOito = 8
+        console.log(NumOito)
+    }
+
+    function nove(){
+        var NumNove = 9
+        console.log(NumNove)
+    }
+
+    function zero(){
+        var NumZero = 0
+        console.log(NumZero)
+    }
+
+    
